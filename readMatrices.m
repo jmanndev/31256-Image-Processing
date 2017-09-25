bcb26d74c797765e4c36e0bf42260a0c40f5cb0a
Felix Gudéhn <Felix@Felixs-MacBook-Air.local>
2017-09-25 12:25:36 +1000

    Add method for parsing the dynamic heat matrices into matlab

---
+++ readMatrices.m
@@ -0,0 +1,16 @@
+% Loads all the heat matrices into memory. The matrices are formatted as
+% 640x480 numbers which correspond to the intensity of the thermal images.
+
+matrixFiles = dir('./Dynamic Heat Matrices/*.txt');      
+nfiles = length(matrixFiles);  % Number of files found
+matrices = {nfiles};
+for ii=1:nfiles
+   currentFilename =  matrixFiles(ii).name;
+   currentMatrixId = fopen(['./Dynamic Heat Matrices/' currentFilename], 'r');
+   formatspec = '%f';
+   currentMatrix = fscanf(currentMatrixId, formatspec);
+   %Change image to proper format and take the inverse e.g. flip col to
+   %row 
+   matrices{ii} = reshape(currentMatrix, 640, [])'; 
+end
+clearvars currentFilename currentMatrixId formatspec ii currentMatrix nfiles matrixFiles
