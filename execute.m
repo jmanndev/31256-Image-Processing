e74ce08c8a7cb7eca1ad1242053cc8f895fed560
Felix Gudéhn <Felix@Felixs-MacBook-Air.local>
2017-09-25 14:33:13 +1000

    Add image error functions, a SURF function registrator and a general version of the execute script

--- execute.m
+++ execute.m
@@ -1,13 +1,14 @@
 % Run script, uses the readFiles and HarrisFunction to iterate over the
 % thermograms and show the resultant alignment of the images 
 
-% load files into memory
+
+function [PROCESSEDIMAGES] = execute(functionName)
 run('readFiles.m');
 
 fixed = images{1};
 figure;
-processedImages = {length(images)};
-processedImages{1} = rgb2gray(fixed);
+PROCESSEDIMAGES = {length(images)};
+PROCESSEDIMAGES{1} = rgb2gray(fixed);
 
 for ii=2:length(images)
     % show initial difference before processing
@@ -17,16 +18,27 @@ for ii=2:length(images)
     before = h.cdata;
 
     % run images through function
-    out = HarrisFunction(moving, fixed);
+    out = callFunction(functionName, moving, fixed);
     imshowpair(fixed, out.RegisteredImage); title('After Processing');
     h = getframe;
     after = h.cdata;
     
     % add to output set
-    processedImages{ii} = out.RegisteredImage;
+    PROCESSEDIMAGES{ii} = out.RegisteredImage;
 
     % display before and after processing images together
     imshowpair(before, after, 'montage'); title('Before / After Processing');
+end
+end
+
+function [IMAGE] = callFunction(functionName, fixed, moving)
+
+if (strcmp(functionName,'Harris'))
+    IMAGE = HarrisFunction(moving, fixed);
+elseif (strcmp(functionName, 'SURF'))
+    IMAGE = SURFFunction(moving, fixed);
+end 
+
 end
 
 
