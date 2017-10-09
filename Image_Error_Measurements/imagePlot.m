5a7d4b58e6af49b28e161d17e6ff159f4fdfd996
J Mann <jmanndev@gmail.com>
2017-10-09 23:00:07 +1100

    Adjust folder locations and comments

--- Image_Error_Measurements/imagePlot.m
+++
@@ -1,9 +0,0 @@
-function imagePlot( imageData, plotRowSize, plotColSize, ...
-                    plotIndex, titleStr )
-%IMAGEPLOT 
-
-subplot(plotRowSize, plotColSize, plotIndex);   
-imshow(imageData, []); title(titleStr);
-
-end
-
--- main.m
+++ main.m
@@ -1,19 +1,20 @@
-% Main file run this you dingo
+%   Main entry function to program
 
 function [originalImages, alignedImages, error] = main(dir) 
 %     if nargin < 1
 %         dir = './Dynamic Thermographic Images/T0004/';
 %     end
     
-    % get images and matrices
+%   Get images and matrices
     dataset = {readFiles(dir)};
     originalImages = dataset;
-    % align images according to method
-%     animate(originalImages);
-   alignedImages = alignImages(dataset);
-  %  animate(alignedImages);
-    % Calculate error based on the shift in images / compared to original
-    % dataset. 
+%   animate(originalImages);
+
+%   Align images according to method
+    alignedImages = alignImages(dataset);
+%   animate(alignedImages);
+%   Calculate error based on the shift in images / compared to original
+%   dataset.
     initial = errorFunction(dataset{1}, 1);
     disp(errorFunction(dataset{1}, initial));
     error = errorFunction(alignedImages{1}, initial)
