33923d3fbdf88b260f9501590bccf87966f6b7bf
HeroicMajor90 <jmug_1996@hotmail.com>
2017-10-09 19:13:17 +1100

    Bug fix for the animate function

--- animate.m
+++ animate.m
@@ -8,14 +8,35 @@ function test = animate( images , times2play, fps)
         fps = 3;
     end
     
-    frames = {};
+    images = images{1};
+    
     for i = 1:length(images)
-        
-
-        frames(i) = im2frame(uint8(images{i}));
+        img = cat(3, images{i}, images{i}, images{i});
+        frames(i) = im2frame(img);
     end
     
     movie(frames, times2play, fps);
     test = true;
 end
 
+% function test = animate( images , times2play, fps)
+% %UNTITLED3 Summary of this function goes here
+% %   Detailed explanation goes here
+%     if (nargin < 2)
+%         times2play = 3;
+%         fps = 3;
+%     elseif (nargin < 3)
+%         fps = 3;
+%     end
+%     pics = images{1};
+%     for i = 1:length(pics)
+% %         map = uint8( 256 * gray(256) );
+% %         rgbImage = ind2rgb(images{i}, map);
+% %         frames(i) = im2frame(pics{i});
+%         img = pics{i};
+%         frames(i) = im2frame(img);
+%     end
+%     
+%     movie(frames, times2play, fps);
+%     test = true;
+% end
